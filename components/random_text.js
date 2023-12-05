b966c67d75e86b8efbbefd99f7a40d9c2ff2ba40
KairanI <kairangame03@gmail.com>
2023-12-05 19:43:25 +0300

    Fast Type 2.0

--- components/random_text.js
+++ components/random_text.js
@@ -1,47 +1,53 @@
 const text = document.getElementById('line');
 const key_animate = document.getElementById('H');
 const caretka = document.getElementById('caretka');
-const text_WPM_ACC = document.getElementById('tittle');
 
 const punct = document.getElementById('punct');
 const numbers = document.getElementById('numbers');
 const en = document.getElementById('en');
 const ru = document.getElementById('ru');
-const activing_lang = document.getElementById('activing_lang');
 const activing_long = document.getElementById('activing');
 const long_10 = document.getElementById('long_10');
 const long_20 = document.getElementById('long_20');
 const long_50 = document.getElementById('long_50');
 const long_80 = document.getElementById('long_80');
 
-// добавляем классы по умолчанию
-activing_long.classList.add('long_10');
-long_10.classList.add('active');
 
-activing_lang.classList.add('en');
-en.classList.add('active');
+const section = document.getElementById('section');
+// добавляем классы по умолчанию
+long_10.classList.add('active_block');
+en.classList.add('active_block');
 
 let text_num = 0;
 let key_num = 0;
 let error_num = 0;
 let sentence = '';
-let caretka_blink = 0;
+let caretka_Interval = 0;
 let timerId = 0;
 let long_text = 10;
+let space_num = 0;
+let num_massiv_space = 0;
+let controller_delate_text = 150;
+let controller_delate_spaceText = 0;
 
-// переменная для разделения большого текста
-let septum_text = '';
+
+// let controller_wordCreate = true;
+
+let controller_space_backspace = true;
+let controller_space_number_characters = true;
+let controller_space = true;
+
 // переменные для подсчета секунд и минут в таймере
 let second = 0;
+
+// контроллер для одного вызова функции WPM
+let controller_wpm = true;
 
 // коонтроллер для удаления и добавления мигания каретке
 let controller_caretka = true;
 
 // переменная для контроля вызова функции caretka-blink, что бы она вызывалась один раз
 let controller_setCaretka = true;
-
-// контроллер для скрытия текста
-let controller_text = true;
 
 // контролеры для анимаций сайт бара
 let controller_punct = 0;
@@ -52,6 +58,10 @@ let controller = 0;
 
 let wpm = 0;
 let acc = 0;
+
+const space_letter = [];
+
+const mod_keys = ['Alt', 'Tab', 'Enter', 'Control', 'Shift', 'CapsLock', 'Meta', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight', 'NumLock', 'Home', 'Clear', 'Insert', 'Delete', 'End', 'PageUp', 'PageDown', 'ScrollLock', 'Pause', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Escape', 'LaunchApplication1', 'LaunchMail', 'MediaPlayPause', 'AudioVolumeUp', 'AudioVolumeDown', 'AudioVolumeMute', 'Backspace'];
 
 const num = [
     4, 234, 567, 89, 123, 890, 432, 76, 543, 12, 345, 678, 90, 98, 765, 432, 123, 456, 789, 9, 678, 90, 987, 654, 321, 345, 11, 90, 98, 765, 432, 123, 1, 789, 45, 678, 90, 987, 654, 2, 345, 678, 90, 98, 765, 432, 8, 456, 789, 45, 678, 90, 7, 654, 321, 345, 678, 90, 98, 765, 432, 123, 10, 789, 45, 678, 90, 3, 654, 321, 345, 12, 90, 98, 765, 432, 123, 456, 789, 45, 678, 90, 6, 654, 321, 5, 678, 90, 98, 765, 432, 123, 456, 789, 45, 678, 90, 987, 88, 321
@@ -107,10 +117,9 @@ function capitalize(word) {
 }
 
 let App = (words) => {
-    clearInterval(caretka_blink);
+    clearInterval(caretka_Interval);
 
     text_num = 0;
-    septum_text = '';
     sentence = '';
     
     for (i = 0; i < long_text; i++) {
@@ -133,12 +142,12 @@ let App = (words) => {
             // когда предыдущий символ является разделителем предложений
             sentence += ' ' + (shouldCapitalize ? capitalize(word) : word);
 
-            if (Math.random() < 0.5 && punct.classList.contains('active_block')) {
-                const number_punctuation = Math.floor(Math.random() * punctuations.length);
-                const punctuation = punctuations[number_punctuation];
+        if (Math.random() < 0.5 && punct.classList.contains('active_block')) {
+            const number_punctuation = Math.floor(Math.random() * punctuations.length);
+            const punctuation = punctuations[number_punctuation];
         
-                sentence += punctuation;
-            }
+            sentence += punctuation;
+        }
     }
 
          // Убираем первый символ, который является пробелом,
@@ -155,105 +164,96 @@ let App = (words) => {
          // Добавляем точку в конец
         if(punct.classList.contains('active_block')) sentence += '.' ;
 
-        if(long_text > 30) {
-            septum_text = sentence.slice(200);
-            sentence = sentence.slice(0, 200);
-            controller_text = false;
-        };
-
-            caretka_blink = setInterval(() => {
-                if (controller_setCaretka == true) {
-                    caretka.classList.add('caretka-blink'); 
-                    controller_setCaretka = false;
-                } else {
-                    caretka.classList.remove('caretka-blink')
-                    controller_setCaretka = true;
-                }
-            }, 550);
-
-        clearInterval(timerId);
-        text_WPM_ACC.classList.remove('tittle-animate');
-        caretka.classList.remove('caretka-type');
-
         acc = 0;
         wpm = 0;
         second = 0
+        space_num = 0;
         error_num = 0;
         key_num = 0;
+        num_massiv_space = 0;
+        space_letter.length = 0;
+        controller_delate_spaceText = 0;
+        controller_delate_text = 150;
+
         controller = 0;
-        controller_text = true;
-
-        text_WPM_ACC.textContent = '';
+        controller_wpm = true;
+        controller_space = true;
+        controller_space_backspace = true;
+        controller_wordCreate = true;
         text.textContent = '';
-        for (let char of sentence) {
-            text.innerHTML += `<letter id="${char + text_num}">${char}</letter>`;
-            ++text_num;
-        };
 
+        returnText();
+        blinkCaretka();
         positionCaretka();
 }
 
 function Keyboard(code, key) {
     window.addEventListener(
         'keydown', (event) => {
-        if (event.code == code && sentence[key_num] == key && sentence.length > key_num) {
-            let id_word = document.getElementById(`${key + key_num}`);
-            id_word.classList.add('wh');
-            ++key_num;
 
-            // включения таймера для WPM
-            if(controller == 0) {
-                timerId = setInterval(() => {
-                    second += 1;
-                } ,1000);
-                controller++;
+        const space_back = document.getElementById(`${space_num} i${key_num}`)
+        if(space_back !== null && space_back !== undefined) controller_space_backspace = true;
+        if(sentence[key_num] == ' ' && space_back === null) controller_space_number_characters = true;
+
+        if(event.code == 'Backspace' && sentence.length > key_num) {
+            const space_word = document.getElementById(`${space_num} i${key_num}`)
+            if(space_word !== null && space_word !== undefined && controller_space_backspace == true) {
+                space_word.remove();
+
+                space_letter[space_letter.length - 1].pop();
+
+                if(space_letter[space_letter.length - 1].length == 0) {
+                    space_letter.pop()
+                }
+                space_num -= 1;
+                controller_space_number_characters = true;
+            } else {
+                let id_word = document.getElementById(`${sentence[key_num - 1] + (key_num - 1)}`);
+                id_word.classList.remove('wh') || id_word.classList.remove('error');
+                if (key_num > 0) key_num -= 1;
+                controller_space = true;
             }
+        }
             
-            // анимация клавиатуры
-            let key_animate = document.getElementById(code);
-            if (key_animate) {
-               key_animate.classList.add('keybord-animate')
-            };
-            setTimeout(() => key_animate.classList.remove('keybord-animate'), 200);
+        else if(sentence[key_num] == ' ' && sentence[key_num] != key && mod_keys.includes(event.key) == false) {
+            if(controller_space_number_characters == false) return;
+            else {
+                const space_word = document.getElementById(`${space_num} i${key_num}`);
+                ++space_num;
+                ++error_num;
+    
+                if(space_word !== null && space_word !== undefined && controller_space == true) {
+                    space_word.insertAdjacentHTML("afterend", `<letter id="${space_num} i${key_num}" class="space_error">${key}</letter>`);
+    
+                    space_letter[space_letter.length - 1].push(key);
+                } else {
+                    const id_word = document.getElementById(`${sentence[key_num - 1] + (key_num - 1)}`);
+                    id_word.insertAdjacentHTML("afterend", `<letter id="${space_num} i${key_num}" class="space_error">${key}</letter>`);
+    
+                    space_letter.push([key]);
+    
+                    controller_space = true;
+                }
+                let key_animate = document.getElementById(code);
+                if (key_animate) {
+                    key_animate.classList.add('keybord-error');
+                };
+                setTimeout(() => {
+                    key_animate.classList.remove('keybord-error')}, 200);
+            }
 
-
-            if(sentence.length > key_num) positionCaretka();
-            clearInterval(caretka_blink);
+            if(space_letter[space_letter.length - 1].length >= 19) controller_space_number_characters = false;
         }
 
-        else if(event.code == 'Backspace') {
-            let id_word = document.getElementById(`${sentence[key_num - 1] + (key_num - 1)}`);
-            id_word.classList.remove('wh') || id_word.classList.remove('error');
-            if (key_num > 0) key_num -= 1;
-
-            if(sentence.length > key_num) positionCaretka();
-        } 
-
-        else if (event.code == code && sentence[key_num] != key && !event.shiftKey && sentence.length > key_num) {
-            let id_word = document.getElementById(`${sentence[key_num] + key_num}`);
-            id_word.classList.add('error');
-            ++key_num;
-            ++error_num;
-
-            // анимация клавиатуры при не правильных нажатиях
-            let key_animate = document.getElementById(code);
-            if (key_animate) {
-               key_animate.classList.add('keybord-error')
-            };
-            setTimeout(() => key_animate.classList.remove('keybord-error'), 200);
-
-            if(sentence.length > key_num) positionCaretka();
-            clearInterval(caretka_blink);
+        else if (sentence.length > key_num && mod_keys.includes(event.key) == false) {
+            if(sentence[key_num] == key) keyType('wh', 'keybord-animate', code);
+            else if(sentence[key_num] != key) keyType('error', 'keybord-error', code);
         }
-        if (sentence.length == key_num && second > 0 && controller_text == true && text_WPM_ACC.textContent == '') WPM();
-        if (sentence.length == key_num && second > 0 && controller_text == false) {
-            key_num = 0;
-            key_word.textContent = '';
 
-            sentence = septum_text;
-            text.textContent = septum_text;
-            controller_text = true;
-        }
+        if(sentence.length == key_num && controller_wpm == true) WPM();
+        if(key_num == (192 - space_num)) delateText();
+
+        if(sentence.length > key_num) positionCaretka();
     },{once:true});;
 }
 
@@ -289,7 +289,6 @@ numbers.addEventListener('click', () => {
         numbers.classList.add('active_block');
         invocation();
         return controller_numbers = 1;
-
     }
 
     if(controller_numbers == 1) {
@@ -302,52 +301,36 @@ numbers.addEventListener('click', () => {
 
 
 ru.addEventListener('click', () => {
-    // удаляем классы
-    activing_lang.classList.remove('en');
-    en.classList.remove('active');
+    en.classList.remove('active_block');
 
-    activing_lang.classList.add('ru');
-    ru.classList.add('active');
+    ru.classList.add('active_block');
 
-    for(let i = 0; i < key_keybord.length; i++) {
-        let a = document.getElementById(key_keybord[i]);
-        a.textContent = keybord_ru[i];
-    }
-
+    ChangingKeys(keybord_ru);
     invocation();
 });
 
 en.addEventListener('click', () => {
     // удаляем классы
-    activing_lang.classList.remove('ru');
-    ru.classList.remove('active');
+    ru.classList.remove('active_block');
 
     // добавляем классы
-    activing_lang.classList.add('en');
-    en.classList.add('active');
+    en.classList.add('active_block');
 
-    for(let i = 0; i < key_keybord.length; i++) {
-        let a = document.getElementById(key_keybord[i]);
-        a.textContent = keybord_en[i];
-    }
-
+    ChangingKeys(keybord_en);
     invocation();
 });
 
 long_10.addEventListener('click', () => {
     // удаляем классы
-    activing_long.classList.remove('long_80');
-    long_80.classList.remove('active');
+    long_80.classList.remove('active_block');
 
-    activing_long.classList.remove('long_50');
-    long_50.classList.remove('active');
+    long_50.classList.remove('active_block');
 
-    activing_long.classList.remove('long_20');
-    long_20.classList.remove('active');
+    long_20.classList.remove('active_block');
 
     // добавляем классы
-    activing_long.classList.add('long_10');
-    long_10.classList.add('active');
+
+    long_10.classList.add('active_block');
 
     long_text = 10;
     invocation();
@@ -355,18 +338,14 @@ long_10.addEventListener('click', () => {
 
 long_20.addEventListener('click', () => {
     // удаляем классы
-    activing_long.classList.remove('long_80');
-    long_80.classList.remove('active');
+    long_80.classList.remove('active_block');
 
-    activing_long.classList.remove('long_50');
-    long_50.classList.remove('active');
+    long_50.classList.remove('active_block');
 
-    activing_long.classList.remove('long_10');
-    long_10.classList.remove('active');
+    long_10.classList.remove('active_block');
 
     // добавляем классы
-    activing_long.classList.add('long_20');
-    long_20.classList.add('active');
+    long_20.classList.add('active_block');
 
     long_text = 20;
     invocation();
@@ -374,18 +353,14 @@ long_20.addEventListener('click', () => {
 
 long_50.addEventListener('click', () => {
     // удаляем классы
-    activing_long.classList.remove('long_80');
-    long_80.classList.remove('active');
+    long_80.classList.remove('active_block');
 
-    activing_long.classList.remove('long_20');
-    long_20.classList.remove('active');
+    long_20.classList.remove('active_block');
 
-    activing_long.classList.remove('long_10');
-    long_10.classList.remove('active');
+    long_10.classList.remove('active_block');
 
-    // добавлем классы
-    activing_long.classList.add('long_50');
-    long_50.classList.add('active');
+    // добавлем кл
+    long_50.classList.add('active_block');
 
     long_text = 50;
     invocation();
@@ -393,22 +368,59 @@ long_50.addEventListener('click', () => {
 
 long_80.addEventListener('click', () => {
     // удаляем классы
-    activing_long.classList.remove('long_50');
-    long_50.classList.remove('active');
+    long_50.classList.remove('active_block');
 
-    activing_long.classList.remove('long_20');
-    long_20.classList.remove('active');
+    long_20.classList.remove('active_block');
 
-    activing_long.classList.remove('long_10');
-    long_10.classList.remove('active');
+    long_10.classList.remove('active_block');
 
     // добавлем классы
-    activing_long.classList.add('long_80');
-    long_80.classList.add('active');
+        
+    long_80.classList.add('active_block');
 
     long_text = 80;
     invocation();
 });
+
+function keyType(classWord, classKey, event_code) {
+    if (timerId < 1)  timerId = setInterval(() => second += 1, 1000);
+
+    let id_word = document.getElementById(`${sentence[key_num] + key_num}`);
+    id_word.classList.add(classWord);
+    ++key_num;
+    if(classWord == 'error') error_num += 1;
+
+    // анимация клавиатуры при не правильных нажатиях
+    let key_animate = document.getElementById(event_code);
+    if (key_animate) {
+       key_animate.classList.add(classKey);
+    };
+    setTimeout(() => {
+        key_animate.classList.remove(classKey)}, 200);
+
+    clearInterval(caretka_Interval);
+    controller_space = false;
+    controller_space_backspace = false;
+}
+
+function blinkCaretka() {
+    caretka_Interval = setInterval(() => {
+        if (controller_setCaretka == true) {
+            caretka.classList.add('caretka-blink'); 
+            controller_setCaretka = false;
+        } else {
+            caretka.classList.remove('caretka-blink')
+            controller_setCaretka = true;
+        }
+    }, 550);
+}
+
+function ChangingKeys(language) {
+    for(let i = 0; i < key_keybord.length; i++) {
+        const change_key = document.getElementById(key_keybord[i]);
+        change_key.textContent = language[i];
+    }
+}
 
 function positionCaretka() {
     let letter = document.getElementById(`${sentence[key_num] + key_num}`);
@@ -418,32 +430,44 @@ function positionCaretka() {
 }
 
 function WPM() {
+    controller_wpm = false;
+    clearInterval(timerId);
+
     acc = Math.floor(100 - (error_num / key_num * 100));
 
     let minutes = second / 60;
     let wpm_1 = Math.floor(long_text / minutes);
 
     wpm = Math.floor(wpm_1 - ((error_num / minutes) / 2));
-    if (wpm < 0) wpm = 0;
+    if (wpm < 0 || isNaN(wpm)) wpm = 0;
     if (acc < 0) acc = 0;
 
-    setTimeout(() => {
-        text_WPM_ACC.textContent = `WPM ${wpm}; ACC ${acc}`;
-        text_WPM_ACC.classList.add('tittle-animate');
-    }, 2000);
-    caretka.classList.add('caretka-type')
+    console.log(`WPM: ${wpm}; ACC: ${acc}`);
+}
 
-    setTimeout(() => {
-        caretka.style.cssText = `
-            left: 1100px;
-            top: 245px;
-        `;
-    }, 3900);
+function returnText() {
+    for (let char of sentence) {
+            text.innerHTML += `<letter id="${char + text_num}">${char}</letter>`;
+            ++text_num;
+    };
+}
+
+function delateText() {
+    for(let i = 40; controller_delate_text > 0; i++) {
+        const id_word = document.getElementById(`${sentence[key_num - i] + (key_num - i)}`);
+        const space_word = document.getElementsByClassName('space_error');
+        for (let elem of space_word) {
+            elem.remove();
+        }
+
+        if(id_word !== null && id_word !== undefined) id_word.remove();
+        controller_delate_text -= 1;
+    }
 }
 
 
 function invocation() {
-    if(ru.classList.contains('active')) {
+    if(ru.classList.contains('active_block')) {
         App(words_ru);
     } else {
         App(words_en);
